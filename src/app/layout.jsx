ff54fef174c6ee0871cadf2f4c5983e5da87ae8f
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-10-14 15:32:46 -0700

    setup of components and hero section text

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,27 @@
-import '@styles/globals.css';
+import '@/styles/globals.css';
+import Nav from '@/components/Nav';
+import Provider from '@/components/Provider';
 
-const layout = () => {
-  return <div></div>;
+const metadata = {
+  title: 'Plantavina',
+  description: 'New Way to Live with Plants',
 };
 
-export default layout;
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,20 @@
+import Feed from '@/components/Feed';
+
 export default function Home() {
   return (
-    <div className="flex justify-center">
-      <h1>Home Page</h1>
-    </div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Plants
+        <br className="max-md:hidden" />
+        <span className="green_gradient text-center"> You Will Love</span>
+      </h1>
+      <p className="desc text-center">
+        Welcome to Plantavina, where you&#39;ll find a handpicked selection of
+        beautiful, healthy plants to bring life and joy to your home—shop now
+        and let nature flourish!
+      </p>
+
+      <Feed />
+    </section>
   );
 }
