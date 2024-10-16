401ff7a8d597a2522d8ae2bc12a7bff7f2aa0579
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-10-15 23:23:32 -0700

    styling logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,18 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          alt="Plantavina Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
