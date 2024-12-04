4c6866505a835e48148b72a6329a255528b6872e
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-12-03 19:16:20 -0800

    setup interfce to ProductView component

--- components/ProductView.tsx
+++ components/ProductView.tsx
@@ -1,5 +1,12 @@
-function ProductView() {
-  return <div>ProductView</div>;
+import { Category, Product } from '@/sanity.types';
+
+interface ProductViewProps {
+  products: Product[];
+  categories: Category[];
 }
+
+const ProductView = ({ products, categories }: ProductViewProps) => {
+  return <div>ProductView</div>;
+};
 
 export default ProductView;
