ed490e30388594596d6bad44e265ee4a7530a9be
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-12-09 20:59:27 -0800

    my cart page created

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,10 +12,14 @@ import {
   useUser,
 } from '@clerk/nextjs';
 import { TrolleyIcon, PackageIcon } from '@sanity/icons';
+import useBasketStore from '@/store/store';
 
 function Header() {
   const { user } = useUser();
   const [isClient, setIsClient] = useState(false);
+  const itemCount = useBasketStore((state) =>
+    state.items.reduce((total, item) => total + item.quantity, 0)
+  );
 
   useEffect(() => {
     setIsClient(true);
@@ -47,6 +51,9 @@ function Header() {
             className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
             <TrolleyIcon className="w-6 h-6" />
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
+              {itemCount}
+            </span>
             <span>My Cart</span>
           </Link>
 
