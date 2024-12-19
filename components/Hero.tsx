7d1ea23fb1b47232b393591a3488b161aca4f6ca
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-12-18 18:52:44 -0800

    working on hero sections styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,8 +5,8 @@ function Hero() {
     <div className="bg-gradient-to-r from-red-600 to-black text-white px-6 py-10 mx-4 my-2 rounded-lg shadow-lg w-full">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex-1">
-          <h2 className="text-3xl sm:tex-5xl font-extrabold text-left mb-4">
-            Welcome to the Best second store in Orange County, CA
+          <h2 className="text-3xl sm:text-5xl font-extrabold text-center mb-4">
+            Second Hand Store in Orange County, California
           </h2>
         </div>
       </div>
